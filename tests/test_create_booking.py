5a31fc4e538e8127e8a9edd138f4e6afc2489a30
IVika-QA <apple@MacBook-Pro-Apple.local>
2024-10-29 23:19:48 +0300

    refactoring test_sozdanie_bronirovaniya_s_nevernymi_dannymi,test_sozdanie_bronirovaniya_s_pustym_JSON,test_sozdanie_bronirovaniya_s_nepolnymi_dannymi,test_create_booking_with_dynamic_dates

--- tests/test_create_booking.py
+++ tests/test_create_booking.py
@@ -39,7 +39,7 @@ def test_creating_booking_with_custom_data(api_client):
     assert response['booking']['additionalneeds'] == booking_data['additionalneeds']
 
 @allure.feature('Test creating booking')
-@allure.story('Negative: creating booking with dynamic dates')
+@allure.story('Positive: creating booking with dynamic dates')
 def test_create_booking_with_dynamic_dates(api_client, booking_dates):
     booking_data = {
         "firstname": "Ivan",
@@ -62,7 +62,7 @@ def test_create_booking_with_dynamic_dates(api_client, booking_dates):
         BookingResponse(**created_booking_response)
     except ValidationError as e:
         raise ValidationError(f"Response validation failed: {e}")
-    # Проверка-2:
+    # Проверка:
     assert created_booking_response['booking']['firstname'] == booking_data['firstname']
     assert created_booking_response['booking']['lastname'] == booking_data['lastname']
     assert created_booking_response['booking']['totalprice'] == booking_data['totalprice']
@@ -72,24 +72,20 @@ def test_create_booking_with_dynamic_dates(api_client, booking_dates):
 
     #Мои добавленные проверки
     # Проверка-3: Сколько времени выполняется запрос?
-    start_time = time.time()
-    created_booking_response = api_client.create_booking(booking_data)
-    elapsed_time = (time.time() - start_time) * 1000  # Переводим в миллисекунды
+    #start_time = time.time()
+    #created_booking_response = api_client.create_booking(booking_data)
+    #elapsed_time = (time.time() - start_time) * 1000  # Переводим в миллисекунды
 
-    print(f"Ответ API при создании бронирования: {created_booking_response}")
-    print(f"Время ответа API: {elapsed_time:.2f} миллисекунд")
+    #print(f"Ответ API при создании бронирования: {created_booking_response}")
+    #print(f"Время ответа API: {elapsed_time:.2f} миллисекунд")
 
     # Проверка-4: Время ответа меньше 1000 миллисекунд?
-    assert elapsed_time < 1000, f"Ответ пришел слишком долго: {elapsed_time:.2f} миллисекунд"
+    #assert elapsed_time < 1000, f"Ответ пришел слишком долго: {elapsed_time:.2f} миллисекунд"
 
-    # Проверка-3: Статус-код ответа 200?
-    status_code = api_client.get_response_status_code(created_booking_response)
-    # Возврращается None
-    #print(status_code)
 
 #Мои тесты
 @allure.feature('Test creating booking')
-@allure.story('Positive:Sozdanie bronirovaniya s nepolnymi dannymi')
+@allure.story('Negative:Creating booking s nepolnymi dannymi')
 def test_sozdanie_bronirovaniya_s_nepolnymi_dannymi(api_client, booking_dates):
     # Не передаю firstname, lastname
     booking_data = {
@@ -105,21 +101,25 @@ def test_sozdanie_bronirovaniya_s_nepolnymi_dannymi(api_client, booking_dates):
     response = api_client.create_booking(booking_data)
 
     # Cервер вернет 400 Bad Request для неполных данных
-    assert response.status_code == 400  # Предполагаем, что сервер вернет 400 Bad Request для неполных данных
+    assert response.status_code == 400
     # В ответе на недостоющее переданное поле вернет слово error
     assert "firstname" in response.json()["error"]
     assert "lastname" in response.json()["error"]
 
+@allure.feature('Test creating booking')
+@allure.story('Negative:Creating booking s pustym JSON')
 def test_sozdanie_bronirovaniya_s_pustym_JSON(api_client,booking_dates):
     # Передаем пустой JSON
     booking_data = {
     }
 
     #Выполняем запрос
-    response = api_client.create_booking(booking_data)
+    response = api_client.create_booking(booking_data
+    #Какие то проверки
 
+@allure.feature('Test creating booking')
+@allure.story('Negative:Creating booking s nevernymi dannymi')
 def test_sozdanie_bronirovaniya_s_nevernymi_dannymi(api_client,booking_dates):
-    # Не передаю firstname, lastname
     booking_data = {
         "totalprice": "150",
         "depositpaid": True,
@@ -131,3 +131,4 @@ def test_sozdanie_bronirovaniya_s_nevernymi_dannymi(api_client,booking_dates):
     }
 
     response = api_client.create_booking(booking_data)
+    #Какие то проверки
