b1995c1b9b78e0676a9ba12b5d286cb5fde8fc50
IVika-QA <apple@MacBook-Pro-Apple.local>
2024-10-30 22:26:26 +0300

    Nothing doing

--- tests/test_create_booking.py
+++ tests/test_create_booking.py
@@ -9,78 +9,81 @@ import time
 @allure.feature('Test creating booking')
 @allure.story('Positive:creating booking with custom data')
 def test_creating_booking_with_custom_data(api_client):
-    booking_data = {
-    "firstname" : "Ivan",
-    "lastname" : "Ivanovich",
-    "totalprice" : 111,
-    "depositpaid" : True,
-    "bookingdates" : {
-        "checkin" : "2025-02-01",
-        "checkout" : "2025-02-10"
-    },
-    "additionalneeds" : "Dinner"
-}
+    with allure.step('Готовим данные для запроса'):
+        booking_data = {
+        "firstname" : "Ivan",
+        "lastname" : "Ivanovich",
+        "totalprice" : 111,
+        "depositpaid" : True,
+        "bookingdates" : {
+            "checkin" : "2025-02-01",
+            "checkout" : "2025-02-10"
+        },
+        "additionalneeds" : "Dinner"
+        }
 
     #Отправили запрос с подготовленными данными с помощью метода - create_booking из api_client
-    response = api_client.create_booking(booking_data)
-    try:
-        #Вызываем класс BookingResponse из файла booking.py и
-        #передаем в него сформированный выше response (kwargs)
-        BookingResponse(**response)
-    except ValidationError as e:
-        raise ValidationError(f"Response validation failed: {e}")
+    with allure.step('Отправляем запррос с подготовленными данными с помощью метода - create_booking из api_client'):
+        response = api_client.create_booking(booking_data)
+        try:
+            #Вызываем класс BookingResponse из файла booking.py и
+            #передаем в него сформированный выше response (kwargs)
+            BookingResponse(**response)
+        except ValidationError as e:
+            raise ValidationError(f"Response validation failed: {e}")
 
-    assert response['booking']['firstname'] == booking_data['firstname']
-    assert response['booking']['lastname'] == booking_data['lastname']
-    assert response['booking']['totalprice'] == booking_data['totalprice']
-    assert response['booking']['depositpaid'] == booking_data['depositpaid']
-    assert response['booking']['bookingdates']['checkin'] == booking_data['bookingdates']['checkin']
-    assert response['booking']['bookingdates']['checkout'] == booking_data['bookingdates']['checkout']
-    assert response['booking']['additionalneeds'] == booking_data['additionalneeds']
+        assert response['booking']['firstname'] == booking_data['firstname']
+        assert response['booking']['lastname'] == booking_data['lastname']
+        assert response['booking']['totalprice'] == booking_data['totalprice']
+        assert response['booking']['depositpaid'] == booking_data['depositpaid']
+        assert response['booking']['bookingdates']['checkin'] == booking_data['bookingdates']['checkin']
+        assert response['booking']['bookingdates']['checkout'] == booking_data['bookingdates']['checkout']
+        assert response['booking']['additionalneeds'] == booking_data['additionalneeds']
 
 @allure.feature('Test creating booking')
 @allure.story('Positive: creating booking with dynamic dates')
 def test_create_booking_with_dynamic_dates(api_client, booking_dates):
-    booking_data = {
-        "firstname": "Ivan",
-        "lastname": "Ivanovich",
-        "totalprice": 150,
-        "depositpaid": True,
-        "bookingdates": {
-            "checkin": booking_dates['checkin'],
-            "checkout": booking_dates['checkout']
-        },
-        "additionalneeds": "Dinner"
-    }
+    with allure.step('Готовим данные для запроса'):
+        booking_data = {
+            "firstname": "Ivan",
+            "lastname": "Ivanovich",
+            "totalprice": 150,
+            "depositpaid": True,
+            "bookingdates": {
+                "checkin": booking_dates['checkin'],
+                "checkout": booking_dates['checkout']
+            },
+            "additionalneeds": "Dinner"
+        }
+    with allure.step('Отправляем запррос с подготовленными данными с помощью метода - create_booking из api_client'):
+        created_booking_response = api_client.create_booking(booking_data)
+        # Проверка-1: Соответствует ли ответ API ожидаемой структуре и типам данных
+        # Вызываем класс BookingResponse из файла booking.py и
+        # передаем в него сформированный выше response:
+        # Две звезды - это значит,что передаем словарь и поэтому-kwargs
+        try:
+            BookingResponse(**created_booking_response)
+        except ValidationError as e:
+            raise ValidationError(f"Response validation failed: {e}")
+        # Проверка:
+        assert created_booking_response['booking']['firstname'] == booking_data['firstname']
+        assert created_booking_response['booking']['lastname'] == booking_data['lastname']
+        assert created_booking_response['booking']['totalprice'] == booking_data['totalprice']
+        assert created_booking_response['booking']['depositpaid'] == booking_data['depositpaid']
+        assert created_booking_response['booking']['bookingdates']['checkin'] == booking_data['bookingdates']['checkin']
+        assert created_booking_response['booking']['bookingdates']['checkout'] == booking_data['bookingdates']['checkout']
 
-    created_booking_response = api_client.create_booking(booking_data)
-    # Проверка-1: Соответствует ли ответ API ожидаемой структуре и типам данных
-    # Вызываем класс BookingResponse из файла booking.py и
-    # передаем в него сформированный выше response:
-    # Две звезды - это значит,что передаем словарь и поэтому-kwargs
-    try:
-        BookingResponse(**created_booking_response)
-    except ValidationError as e:
-        raise ValidationError(f"Response validation failed: {e}")
-    # Проверка:
-    assert created_booking_response['booking']['firstname'] == booking_data['firstname']
-    assert created_booking_response['booking']['lastname'] == booking_data['lastname']
-    assert created_booking_response['booking']['totalprice'] == booking_data['totalprice']
-    assert created_booking_response['booking']['depositpaid'] == booking_data['depositpaid']
-    assert created_booking_response['booking']['bookingdates']['checkin'] == booking_data['bookingdates']['checkin']
-    assert created_booking_response['booking']['bookingdates']['checkout'] == booking_data['bookingdates']['checkout']
+        #Мои добавленные проверки
+        # Проверка-3: Сколько времени выполняется запрос?
+        #start_time = time.time()
+        #created_booking_response = api_client.create_booking(booking_data)
+        #elapsed_time = (time.time() - start_time) * 1000  # Переводим в миллисекунды
 
-    #Мои добавленные проверки
-    # Проверка-3: Сколько времени выполняется запрос?
-    #start_time = time.time()
-    #created_booking_response = api_client.create_booking(booking_data)
-    #elapsed_time = (time.time() - start_time) * 1000  # Переводим в миллисекунды
+        #print(f"Ответ API при создании бронирования: {created_booking_response}")
+        #print(f"Время ответа API: {elapsed_time:.2f} миллисекунд")
 
-    #print(f"Ответ API при создании бронирования: {created_booking_response}")
-    #print(f"Время ответа API: {elapsed_time:.2f} миллисекунд")
-
-    # Проверка-4: Время ответа меньше 1000 миллисекунд?
-    #assert elapsed_time < 1000, f"Ответ пришел слишком долго: {elapsed_time:.2f} миллисекунд"
+        # Проверка-4: Время ответа меньше 1000 миллисекунд?
+        #assert elapsed_time < 1000, f"Ответ пришел слишком долго: {elapsed_time:.2f} миллисекунд"
 
 
 #Мои тесты
@@ -88,17 +91,18 @@ def test_create_booking_with_dynamic_dates(api_client, booking_dates):
 @allure.story('Negative:Creating booking s nepolnymi dannymi')
 def test_sozdanie_bronirovaniya_s_nepolnymi_dannymi(api_client, booking_dates):
     # Не передаю firstname, lastname
-    booking_data = {
-        "totalprice": 150,
-        "depositpaid": True,
-        "bookingdates": {
-            "checkin": booking_dates['checkin'],
-            "checkout": booking_dates['checkout']
-        },
-        "additionalneeds": "Dinner"
-    }
-
-    response = api_client.create_booking(booking_data)
+    with allure.step('Готовим данные для запроса:Не передаю firstname, lastname'):
+        booking_data = {
+            "totalprice": 150,
+            "depositpaid": True,
+            "bookingdates": {
+                "checkin": booking_dates['checkin'],
+                "checkout": booking_dates['checkout']
+            },
+            "additionalneeds": "Dinner"
+        }
+    with allure.step('Отправляем запррос с подготовленными данными с помощью метода - create_booking из api_client'):
+        response = api_client.create_booking(booking_data)
 
     # Cервер вернет 400 Bad Request для неполных данных
     assert response.status_code == 400
@@ -110,25 +114,28 @@ def test_sozdanie_bronirovaniya_s_nepolnymi_dannymi(api_client, booking_dates):
 @allure.story('Negative:Creating booking s pustym JSON')
 def test_sozdanie_bronirovaniya_s_pustym_JSON(api_client,booking_dates):
     # Передаем пустой JSON
-    booking_data = {
-    }
+    with allure.step('Готовим данные для запроса:Передаем пустой запрос JSON'):
+        booking_data = {
+        }
 
     #Выполняем запрос
-    response = api_client.create_booking(booking_data
+    with allure.step('Отправляем запрос с пустым JSON с помощью метода - create_booking из api_client'):
+        response = api_client.create_booking(booking_data)
     #Какие то проверки
 
 @allure.feature('Test creating booking')
 @allure.story('Negative:Creating booking s nevernymi dannymi')
 def test_sozdanie_bronirovaniya_s_nevernymi_dannymi(api_client,booking_dates):
-    booking_data = {
-        "totalprice": "150",
-        "depositpaid": True,
-        "bookingdates": {
-            "checkin": booking_dates['checkin'],
-            "checkout": booking_dates['checkout']
-        },
-        "additionalneeds": "Dinner"
-    }
-
-    response = api_client.create_booking(booking_data)
-    #Какие то проверки
+    with allure.step('Готовим данные для запроса: В поле totalprice передаю строку вместо числа'):
+        booking_data = {
+            "totalprice": "150",
+            "depositpaid": True,
+            "bookingdates": {
+                "checkin": booking_dates['checkin'],
+                "checkout": booking_dates['checkout']
+            },
+            "additionalneeds": "Dinner"
+        }
+    with allure.step('Отправляем запрос с неверными данными: В поле totalprice кладем строку вместо числа с помощью метода - create_booking из api_client'):
+        response = api_client.create_booking(booking_data)
+        #Какие то проверки
